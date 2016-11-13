131d2874513f7878da8714547f4309c32474adc2
Joshua White <lamarwhitej@yahoo.com>
2016-11-13 22:08:15 +0000

    Added files for pushing objects to ES environment.

---
+++ import_objects.py
@@ -0,0 +1,48 @@
+import json
+import requests
+import argparse
+
+from ConfigParser import SafeConfigParser
+
+# Initialize Config Variables
+config = SafeConfigParser()
+config.read("elk.cnf")
+host = config.get("kibana", "host")
+visuals_json = config.get("kibana", "visualizations")
+dashboards_json = config.get("kibana", "dashboards")
+searches_json = config.get("kibana", "searches")
+
+
+
+with open(visuals_json) as data_file:
+    print "Visualizations Start"
+    data = json.load(data_file)
+
+for i in data:
+    name = i['_id']
+    data = json.dumps(i['_source'])
+    url = host + 'visualization/' + name
+    response = requests.put(url, data=data)
+    print 'Visualization Status: ' + str(response)
+
+with open(dashboards_json) as data_file:
+    print "Dashboard Start"
+    data = json.load(data_file)
+
+for i in data:
+    name = i['_id']
+    data = json.dumps(i['_source'])
+    url = host + 'dashboard/' + name
+    response = requests.put(url, data=data)
+    print 'Dashboard Status: ' + str(response)
+
+with open(searches_json) as data_file:
+    print "Searches Start"
+    data = json.load(data_file)
+
+for i in data:
+    name = i['_id']
+    data = json.dumps(i['_source'])
+    url = host + 'search/' + name
+    response = requests.put(url, data=data)
+    print 'Search Status: ' + str(response)
