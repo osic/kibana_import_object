503e82d981459c652bc5649af7bba219fed061c0
Joshua White <joshua.l.white@intel.com>
2016-11-14 08:58:23 -0800

    Added command line args.

--- import_objects.py
+++ import_objects.py
@@ -4,46 +4,80 @@ import argparse
 
 from ConfigParser import SafeConfigParser
 
-# Initialize Config Variables
-config = SafeConfigParser()
-config.read("elk.cnf")
-host = config.get("kibana", "host")
-visuals_json = config.get("kibana", "visualizations")
-dashboards_json = config.get("kibana", "dashboards")
-searches_json = config.get("kibana", "searches")
 
-#Import Visualizations
-with open(visuals_json) as data_file:
-    print "Visualizations Start"
-    data = json.load(data_file)
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self):
+        desc = "Accepts search, dashboard, and visualization json from Kibana.  Pushes to ES."
+        usage_string = "[-s/--search] ([-d/--dashboard] | [-v/--visual]) [-o/--output-file]"
 
-for i in data:
-    name = i['_id']
-    data = json.dumps(i['_source'])
-    url = host + 'visualization/' + name
-    response = requests.put(url, data=data)
-    print 'Visualization Status: ' + str(response)
+        super (ArgumentParser, self).__init__(
+            usage=usage_string, description=desc)
 
-#Import Dashboards
-with open(dashboards_json) as data_file:
-    print "Dashboard Start"
-    data = json.load(data_file)
+        self.add_argument(
+            "-s", "--search", metavar="<json file with searches>",
+            required=False, default=None)
+            
+        self.add_argument(
+            "-d", "--dashboard", metavar="<json file with dashboards>",
+            required=False, default=None)
+            
+        self.add_argument(
+            "-v", "--visual", metavar="<json file with visualizations>",
+            required=False, default=None)
 
-for i in data:
-    name = i['_id']
-    data = json.dumps(i['_source'])
-    url = host + 'dashboard/' + name
-    response = requests.put(url, data=data)
-    print 'Dashboard Status: ' + str(response)
+        self.add_argument(
+            "-o", "--output-file", metavar="<path to output file>",
+            required=False, default=None)
 
-#Import Searches
-with open(searches_json) as data_file:
-    print "Searches Start"
-    data = json.load(data_file)
 
-for i in data:
-    name = i['_id']
-    data = json.dumps(i['_source'])
-    url = host + 'search/' + name
-    response = requests.put(url, data=data)
-    print 'Search Status: ' + str(response)
+def entry_point():
+
+    cl_args = ArgumentParser().parse_args()
+
+    # Initialize Config Variables
+    config = SafeConfigParser()
+    config.read("elk.cnf")
+    host = cl_args.output_file or config.get("kibana", "host")
+    visuals_json = cl_args.visual or config.get("kibana", "visualizations")
+    dashboards_json = cl_args.dashboard or config.get("kibana", "dashboards")
+    searches_json = cl_args.search or config.get("kibana", "searches")
+
+    #Import Visualizations
+    with open(visuals_json) as data_file:
+        print "Visualizations Start"
+        data = json.load(data_file)
+
+    for i in data:
+        name = i['_id']
+        data = json.dumps(i['_source'])
+        url = host + 'visualization/' + name
+        response = requests.put(url, data=data)
+        print 'Visualization Status: ' + str(response)
+
+    #Import Dashboards
+    with open(dashboards_json) as data_file:
+        print "Dashboard Start"
+        data = json.load(data_file)
+
+    for i in data:
+        name = i['_id']
+        data = json.dumps(i['_source'])
+        url = host + 'dashboard/' + name
+        response = requests.put(url, data=data)
+        print 'Dashboard Status: ' + str(response)
+
+    #Import Searches
+    with open(searches_json) as data_file:
+        print "Searches Start"
+        data = json.load(data_file)
+
+    for i in data:
+        name = i['_id']
+        data = json.dumps(i['_source'])
+        url = host + 'search/' + name
+        response = requests.put(url, data=data)
+        print 'Search Status: ' + str(response)
+
+
+if __name__ == "__main__":
+    entry_point()
